f5f8f37147df263204a6d5c6550a1dc29fe50c20
Martín Rodríguez <monse.developer@gmail.com>
2024-02-21 18:02:06 -0600

    API con db

---
+++ NetCoreAPI (db)/NetCoreAPI/Controllers/ClienteController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreAPI.Models;
+using System.Security.Claims;
+
+namespace NetCoreAPI.Controllers
+{
+    //dominio/cliente/listar
+    [ApiController]
+    [Route("cliente")]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+            //Todo el codigo retornando algo
+            List<ClienteModel> clientes = new List<ClienteModel>
+            {
+                new ClienteModel
+                {
+                    id = "1",
+                    correo = "google@gmail.com",
+                    edad = "19",
+                    nombre = "Bernardo Peña"
+                },
+                new ClienteModel
+                {
+                    id = "2",
+                    correo = "miguelgoogle@gmail.com",
+                    edad = "23",
+                    nombre = "Miguel Mantilla"
+                }
+            };
+           return clientes;
+            
+        }
+        [HttpGet]
+        [Route("listarxid")]
+        public dynamic listarClientexid(int codigo)
+        {
+
+            //obtienes en cliente dB
+            return new ClienteModel
+            {
+                id = codigo.ToString(),
+                correo = "google@gmail.com",
+                edad = "19",
+                nombre = "Bernardo Peña"
+            };
+
+        }
+
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic guardarCliente(ClienteModel cliente)
+        {
+            //Guardar en la db y le asignas un id
+            cliente.id = "3";
+
+            return new
+            {
+                success = true,
+                message = "Cliente Resgistrado",
+                result = cliente
+            };
+        }
+
+        [HttpPost]
+        [Route("eliminar")]
+        [Authorize]//para que si o si me envie un token valido(para ahorar recursos)
+        public dynamic eliminarCliente(ClienteModel cliente)
+        {
+            //obtener el token
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            //respuesta token
+            var rToken = Jwt.validarToken(identity);
+
+            if (!rToken.success) return rToken;
+
+            //
+            UsuarioModel usuario = rToken.result;
+            if (usuario.rol != "administrador") 
+            {
+                return new
+                {
+                    success = false,
+                    message = "No tienes permisos para eliminar clientes",
+                    result = ""
+                };
+            }
+
+            /*Validacion simple
+            string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+            //eliminas en la db
+
+            if (token != "marco123.")
+            {
+                return new
+                {
+                    success = false,
+                    message = "token incorrecto",
+                    result = ""
+                };
+            }
+            */
+            return new
+            {
+                success = true,
+                message = "cliente eliminado",
+                result = cliente
+            };
+
+        }
+    }
+}
---
+++ NetCoreAPI (db)/NetCoreAPI/Program.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//Si queremos add un btn para colocar modificamos ->
+//->builder.Services.AddSwaggerGen();
+//----
+builder.Services.AddSwaggerGen(c => {
+    // Titulo (Dise�o)
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Software Monns", Version = "v1" });
+
+    //Boton Autorize (Swagger)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Jwt Authorization",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+       {
+           new OpenApiSecurityScheme
+           {
+               Reference = new OpenApiReference
+               {
+                   Type = ReferenceType.SecurityScheme,
+                   Id = "Bearer"
+               }
+           },
+           new string[] {}
+       }
+    });
+});
+//-------
+
+
+//
+//Configuracion de conexion con tokens
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+
+
+var app = builder.Build();
+
+
+//
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// agrega Aut
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
